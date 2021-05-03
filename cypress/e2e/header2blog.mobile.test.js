7ea4408b7e7b7b6e1d63f7065b3eabb3e2eaf702
Amadeus <38767929+Mozart409@users.noreply.github.com>
2021-05-03 16:19:45 +0200

    fix: :white_check_mark: updated test

--- cypress/e2e/header2blog.mobile.test.js
+++ cypress/e2e/header2blog.mobile.test.js
@@ -3,19 +3,20 @@
 describe('Test header on mobile', () => {
   beforeEach(() => {
     cy.viewport('iphone-6')
+    cy.visit('http://localhost:3000/')
+    //   cy.get('[data-testid=uc-close-button]').click()
   })
 
   it('open mobilemenu and test if menuitems are visible', function () {
-    cy.visit('http://localhost:3000/')
     cy.get('[data-cy=MobileMenuButton]').click()
     cy.get('a').contains('Tesla mieten')
     cy.get('a').contains('FAQ')
     cy.get('a').contains('Kontakt')
     cy.get('a').contains('+49 711 49 06 64 50')
-    cy.get('.px-2 > [href="/blog/"]').click()
   })
 
   it('Checking Blog Page', () => {
+    cy.visit('http://localhost:3000/blog/')
     cy.url().should('contain', '/blog/')
     cy.contains('Blog')
     cy.get('.BlogTitle').its('length').should('be.gte', 1)

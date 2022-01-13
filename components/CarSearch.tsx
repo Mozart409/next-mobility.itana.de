b684e68bd6fff0eca40e5de054c5f1a55be59c5f
Amadeus <38767929+Mozart409@users.noreply.github.com>
2022-01-13 19:24:58 +0100

    fix: :bug: removed unused imports

--- components/CarSearch.tsx
+++ components/CarSearch.tsx
@@ -1,7 +1,5 @@
 import { ReactElement, useState } from 'react'
 import { addDays, format } from 'date-fns'
-import ListBox from './ListBox'
-import { de } from 'date-fns/locale'
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -23,17 +21,16 @@ function CarSearch({}: Props): ReactElement {
   return (
     <div>
       <ul role="list" className="space-y-3">
-        {console.log(pickUpTime)}
         <h2>Buchungszeitraum</h2>
         <div className="grid grid-cols-1 md:grid-cols-2">
-          <li className="bg-white shadow overflow-hidden px-4 py-4 sm:px-6 sm:rounded-md">
+          <li className="overflow-hidden py-4 px-4 bg-white shadow sm:px-6 sm:rounded-md">
             {/* Your content */}
             <label htmlFor="pickUpDate">Von </label>
             <input
               type="date"
               id="pickUpDate"
               name="pickUpDate"
-              className="shadow-sm focus:ring-itana-red/50 focus:border-itana-red/50 block w-full sm:text-sm border-gray-300 rounded-md"
+              className="block w-full rounded-md border-gray-300 shadow-sm sm:text-sm focus:ring-itana-red/50 focus:border-itana-red/50"
               onChange={(e) => setPickUpDate(e.target.value)}
               value={pickUpDate}
               min={todayDays}
@@ -41,13 +38,13 @@ function CarSearch({}: Props): ReactElement {
             />
           </li>
 
-          <li className="bg-white shadow overflow-hidden px-4 py-4 sm:px-6 sm:rounded-md">
+          <li className="overflow-hidden py-4 px-4 bg-white shadow sm:px-6 sm:rounded-md">
             <label htmlFor="returnDate">Bis </label>
             <input
               type="date"
               id="returnDate"
               name="returnDate"
-              className="shadow-sm focus:ring-itana-red/50 focus:border-itana-red/50 block w-full sm:text-sm border-gray-300 rounded-md"
+              className="block w-full rounded-md border-gray-300 shadow-sm sm:text-sm focus:ring-itana-red/50 focus:border-itana-red/50"
               onChange={(e) => setReturnDate(e.target.value)}
               value={returnDate}
               min={todayDays}
@@ -55,26 +52,26 @@ function CarSearch({}: Props): ReactElement {
             />
           </li>
 
-          <li className="bg-white shadow overflow-hidden px-4 py-4 sm:px-6 sm:rounded-md">
+          <li className="overflow-hidden py-4 px-4 bg-white shadow sm:px-6 sm:rounded-md">
             <label htmlFor="pickUpTime">Abholzeit</label>
             <input
               type="time"
               id="pickUpTime"
               name="pickUpTime"
-              className="shadow-sm focus:ring-itana-red/50 focus:border-itana-red/50 block w-full sm:text-sm border-gray-300 rounded-md"
+              className="block w-full rounded-md border-gray-300 shadow-sm sm:text-sm focus:ring-itana-red/50 focus:border-itana-red/50"
               onChange={(e) => setPickUpTime(e.target.value)}
               value={pickUpTime}
               required
             />
           </li>
 
-          <li className="bg-white shadow overflow-hidden px-4 py-4 sm:px-6 sm:rounded-md">
+          <li className="overflow-hidden py-4 px-4 bg-white shadow sm:px-6 sm:rounded-md">
             <label htmlFor="returnTime">Rückgabezeit</label>
             <input
               type="time"
               id="returnTime"
               name="returnTime"
-              className="shadow-sm focus:ring-itana-red/50 focus:border-itana-red/50 block w-full sm:text-sm border-gray-300 rounded-md"
+              className="block w-full rounded-md border-gray-300 shadow-sm sm:text-sm focus:ring-itana-red/50 focus:border-itana-red/50"
               onChange={(e) => setReturnTime(e.target.value)}
               value={returnTime}
               required
@@ -82,7 +79,7 @@ function CarSearch({}: Props): ReactElement {
           </li>
         </div>
 
-        <li className="bg-white shadow overflow-hidden px-4 py-4 sm:px-6 sm:rounded-md">
+        <li className="overflow-hidden py-4 px-4 bg-white shadow sm:px-6 sm:rounded-md">
           <a
             href={`https://itana.rentware.io/?fromTime=${pickUpTime}&toTime=${returnTime}&fromDate=${pickUpDate}&toDate=${returnDate}&tab=0`}
             target="_blank"
@@ -91,7 +88,7 @@ function CarSearch({}: Props): ReactElement {
             {/*  "https://itana.rentware.io/?fromTime=08:00&toTime=17:00&fromDate=2022-01-14&toDate=2022-01-14&tab=0" */}
             <button
               type="button"
-              className="inline-flex items-center py-3 px-6 w-full text-base font-medium text-white rounded-full border border-transparent focus:ring-2 focus:ring-offset-2 focus:outline-none bg-itana-red/75 hover:bg-itana-red/60 focus:ring-itana-red shadow-lg shadow-itana-red/50"
+              className="inline-flex items-center py-3 px-6 w-full text-base font-medium text-white rounded-full border border-transparent shadow-lg focus:ring-2 focus:ring-offset-2 focus:outline-none bg-itana-red/75 shadow-itana-red/50 hover:bg-itana-red/60 focus:ring-itana-red"
             >
               <span className="text-center">Auto suchen</span>
             </button>

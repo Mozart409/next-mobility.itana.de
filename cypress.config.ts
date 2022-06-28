b88bc203e0546da9a1b78d1d34b981547bbb7bb5
Mozart409 <maderamadeus@gmail.com>
2022-06-28 21:33:19 +0200

    test: :construction: cypress does not work

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,21 +1,16 @@
 import {defineConfig} from 'cypress';
 
 export default defineConfig({
-  video: false,
   projectId: '5k6fpq',
-  chromeWebSecurity: false,
-  screenshotOnRunFailure: false,
+  video: false,
+  screenshotOnRunFailure: true,
   trashAssetsBeforeRuns: true,
-
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-
-    baseUrl: 'http://localhost:3000/',
+    baseUrl: 'http://localhost:3000',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   },
-
   component: {
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
     devServer: {
       framework: 'next',
       bundler: 'webpack',
--- cypress/component/BookingForm.cy.ts
+++
@@ -1,5 +0,0 @@
-describe('ComponentName-copy-1.cy.ts', () => {
-  it('playground', () => {
-    // cy.mount()
-  })
-})
